8785c0fb0ddcab280068476050a0ffee52f4df81
PhilipLeezy <9philip@gmail.com>
2017-02-08 11:58:28 -0800

    Update double_or_nothing.py

--- double_or_nothing.py
+++ double_or_nothing.py
@@ -21,7 +21,10 @@ while play == "yes":
         if (play == "yes"):
             pot = (pot * 2)
             print("The pot is doubled: $" + str(pot) + ".00")
-        else:
+        if (play == "no"):
             print("Your winnings: $" + str(pot) + ".00")
+        else:
+            while play not in [yes,no]:
+                play = input("I'm sorry, I don't understand. Please respond with yes or no: ")
     else:
         print("You lose...")
