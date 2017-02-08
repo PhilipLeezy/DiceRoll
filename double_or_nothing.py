0ff25abe6567f5ca8f411517e41bb5f48e961a91
jyhh2 <huij93@gmail.com>
2017-02-08 23:09:59 +0000

    Update double_or_nothing.py

--- double_or_nothing.py
+++ double_or_nothing.py
@@ -2,7 +2,7 @@ from random import randint
 import sys
 
 name = input("What's your name? ")
-print("Nice to meet you " + name + "!")
+print("Nice to meet you " + str(name) + "!")
 
 play = "yes"
 pot = 5
@@ -24,10 +24,10 @@ while play == "yes":
         elif (play == "no"):
             print("Your winnings: $" + str(pot) + ".00")
         else:
-            while play not in [yes,no]:
+            while play not in ["yes","no"]:
                 play = input("I'm sorry, I don't understand. Please respond with yes or no: ")
     else:
         print("You lose...")
         play = input("Play a new round? ")
-        while play not in [yes,no]:
-            play = input("I'm sorry, I don't understand. Please respond with yes or no: ")
+        while play not in ["yes","no"]:
+            play = input("I'm sorry, I don't understand. Please respond with \"yes\" or \"no\": ")
