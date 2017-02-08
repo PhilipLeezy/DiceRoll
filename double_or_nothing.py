9e576b2dc5c6643bb194d6d09e9fd719e0238541
PhilipLeezy <9philip@gmail.com>
2017-02-08 11:49:23 -0800

    Update double_or_nothing.py

--- double_or_nothing.py
+++ double_or_nothing.py
@@ -5,10 +5,21 @@ name = input("What's your name? ")
 print("Nice to meet you " + name + "!")
 
 play = "yes"
-wager = input("Enter your initial wager: ")
+pot=5
 
 while play == "yes":
     i=1
+    p1=randint(1,6)
     p2=randint(1,6)
-    print("Round " + str(i)
-    print("Player 2 rolls: " + str(p2))
+    print("Round " + str(i))
+    print("Amount in Pot: $" + str(pot) + ".00")
+    print("The house rolls: " + str(p2))
+    print("You roll: " + str(p1))
+    if (p1 > p2):
+        print("You win!")
+        pot = (pot * 2)
+        print("The pot is doubled: $" + str(pot) + ".00")
+        play = input("Continue? ")
+        if (play == "no"):
+            print("Your winnings: $" + str(pot) + ".00")
+            
