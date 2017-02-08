0807ead4dfade4dd55874eb019802722903be3e6
PhilipLeezy <9philip@gmail.com>
2017-02-08 11:10:49 -0800

    Create double_or_nothing.sql

---
+++ double_or_nothing.sql
@@ -0,0 +1,9 @@
+from random import randint
+import sys
+
+name = input("What's your name? ")
+print("Nice to meet you " + name + "!")
+
+play = input("Would you like to play? ")
+
+while play = "yes"
