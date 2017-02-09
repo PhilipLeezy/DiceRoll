d44347b1d61485fedc52e61f6931b3d851aac067
PhilipLeezy <9philip@gmail.com>
2017-02-09 15:37:31 -0800

    Update strip_delimiters.py

--- strip_delimiters.py
+++ strip_delimiters.py
@@ -1,10 +1,10 @@
 import csv
 import glob
+import os
 
-filenames = glob.glob('*.txt')
+filenames = [f.replace('.txt', '') for f in [os.path.basename(x) for x in glob.glob('*.txt')]]
 
 for file in filenames:
-        file = file.replace('.txt', '')
         with open(file + '.txt', 'rt') as input:
                 with open(file + '_output.txt', 'wt') as output:
                         for line in input:
